900031a27a31e0470e83200c3ec6a0564b7b5b07
zakaria <kharroubzakaria32@gmail.com>
2025-01-16 22:05:55 +0100

    create city and fix error

--- src/main/java/com/example/city/web/vm/CityVM.java
+++ src/main/java/com/example/city/web/vm/CityVM.java
@@ -1,19 +1,11 @@
 package com.example.city.web.vm;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 
 @Data
 public class CityVM {
-    @JsonProperty("name")
     private String name;
-
-    @JsonProperty("region")
     private String region;
-
-    @JsonProperty("country")
     private String country;
-
-
 }
