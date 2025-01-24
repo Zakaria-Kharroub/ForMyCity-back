2a0d33bac3186019ce1eb52899ebffe426b36751
zakaria <kharroubzakaria32@gmail.com>
2025-01-25 00:15:24 +0100

    config for cors error

--- src/main/java/com/example/city/web/rest/CityController.java
+++ src/main/java/com/example/city/web/rest/CityController.java
@@ -7,13 +7,14 @@ import com.example.city.web.vm.CityVM;
 import com.example.city.web.vm.mapper.CityMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/cities")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CityController {
     private final CityService cityService;
     private CityMapper cityMapper;
@@ -30,4 +31,15 @@ public class CityController {
         CityDTO cityDTO = cityMapper.toDTO(savedCity);
         return new ResponseEntity<>(cityDTO, HttpStatus.CREATED);
     }
+
+    @GetMapping("/")
+    public ResponseEntity<List<CityDTO>> findAll(){
+        List<City> cities = cityService.findAll();
+        List<CityDTO> cityDTOS = cities.stream()
+                .map(cityMapper::toDTO)
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(cityDTOS,HttpStatus.OK);
+    }
+
+
 }
